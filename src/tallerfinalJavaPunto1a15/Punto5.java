d3814059dc491a930f5b32a93621159aa6de92e9
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-13 09:44:39 -0500

    Se entrega punto 5 funcional

---
+++ src/tallerfinalJavaPunto1a15/Punto5.java
@@ -0,0 +1,15 @@
+package tallerfinalJavaPunto1a15;
+
+/**TODO:Muestra los números impares y pares del 1 al 100 (ambos incluidos). Usa un bucle while.
+ * @author Fernando Quiceno Gomez
+ */
+public class Punto5 {
+    public static void main(String[] args) {
+
+        int iterador = 1;
+        while(iterador<=100){
+            System.out.println(iterador);
+            iterador++;
+        }
+    }
+}
