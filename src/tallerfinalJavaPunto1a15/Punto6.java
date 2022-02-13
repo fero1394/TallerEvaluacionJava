57568b277fb629970a1bfbf1b064157104503a5b
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-13 09:47:41 -0500

    Se entrega punto 6 funcional

---
+++ src/tallerfinalJavaPunto1a15/Punto6.java
@@ -0,0 +1,15 @@
+package tallerfinalJavaPunto1a15;
+
+/**TODO:Realiza el ejercicio anterior usando un ciclo for.
+ * @author Fernando Quiceno Gomez
+ */
+
+public class Punto6 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=100;i++){
+            System.out.println(i);
+        }
+
+    }
+}
