36c0002fce9d3802621a38c0e0fa4f1ea9d0652a
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-14 17:33:41 -0500

    se entrega punto 16 funcional

--- src/punto16/tallerfinal/sofka/Persona.java
+++ src/punto16/tallerfinal/sofka/Persona.java
@@ -1,5 +1,7 @@
 package punto16.tallerfinal.sofka;
 
+import java.util.Random;
+
 public class Persona {
     /**
      * Atributos Persona
@@ -7,7 +9,7 @@ public class Persona {
     private String nombre = "";
     private int edad=0;
     private int DNI;
-    private char sexo = 'H';
+    private String sexo="H";
     private double peso=0;
     private double altura=0;
 
@@ -15,18 +17,20 @@ public class Persona {
      * Constructores
      */
     public Persona(){
-
+        generaDNI();
     }
 
-    public Persona(String nombre,int edad){
+    public Persona(String nombre,int edad,String sexo){
         this.nombre = nombre;
         this.edad = edad;
+        generaDNI();
+        comprobarSexo(sexo);
     }
 
-    public Persona(String nombre,int edad,int DNI,char sexo,double peso,double altura){
+    public Persona(String nombre,int edad,String sexo,double peso,double altura){
         this.nombre = nombre;
         this.edad = edad;
-        this.DNI = DNI;
+        generaDNI();
         comprobarSexo(sexo);
     }
 
@@ -34,13 +38,20 @@ public class Persona {
      * Metodos
      */
 
-    public int calcularIMC(int peso,int altura){
+    /**
+     * segun las constantes establecidas dentro del metodo retorna su condicion del peso
+     * @param peso
+     * @param altura
+     * @return peso ideal
+     */
+
+    public int calcularIMC(double peso,double altura){
 
         final int PESOIDEAL = -1;
         final int PORDEBAJOPESOIDEAL = 0;
         final int SOBREPESO = 1;
 
-        int imc = peso/(altura^2);
+        int imc = (int)peso/((int)altura^2);
 
         if(imc<20){
             return PESOIDEAL;
@@ -51,6 +62,10 @@ public class Persona {
         }
     }
 
+    /**
+     * calcula si es mayor de edad
+     * @return true si es mayor de edad de lo contrario false
+     */
     public boolean esMayorDeEdad(){
         if(edad>=18){
             return true;
@@ -60,13 +75,77 @@ public class Persona {
         }
     }
 
-    private void comprobarSexo(char sexo){
-        char sexoMayuscula = Character.toUpperCase(sexo);
+    /**
+     * Modifica sexo si parametro es M de lo contrario lo
+     * deja por defecto que es H
+     * @param sexo
+     */
+    private void comprobarSexo(String sexo){
+        String sexoMayuscula = sexo.toUpperCase();
 
-        if(sexoMayuscula == 'M'){
-            this.sexo = 'M';
+        if(sexoMayuscula.equalsIgnoreCase("M")){
+            this.sexo = "M";
+        }else{
+            this.sexo = "H";
         }
     }
 
+    /**
+     * Devuelve una cadena de texto con toda la informacion
+     * de el objeto instanciado con esta clase
+     * @return cadena de texto con la informacion de los atributos
+     */
+    @Override
+    public String toString(){
+        return "El nombre de la persona es: "+nombre+" La edad es: "+edad+" El DNI es: "+DNI+" El sexo es: "+sexo+" Su peso es: "+peso+" Su altura: "+altura;
+    }
 
+    /**
+     * genera un numero DNI para asignarselo al objeto instanciado
+     * con esta clase el rango se establece de ocho cifras
+     */
+    private void generaDNI(){
+        Random claseRandom = new Random();
+        int randomDNI = claseRandom.nextInt(99999999 - 00000000);
+        this.DNI = randomDNI;
+    }
+
+    /**
+     * los siguientes son una serie de metodos set de los atributos
+     * @param nombre
+     */
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void setSexo(String sexo){
+        comprobarSexo(sexo);
+    }
+
+    public void setPeso(double peso){
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura){
+        this.altura = altura;
+    }
+
+    /**
+     * Los siguientes son metodos getters de los valores que se necesita acceder a su valor
+     */
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public double getPeso(){
+        return this.peso;
+    }
+
+    public double getAltura(){
+        return this.altura;
+    }
 }
---
+++ src/punto16/tallerfinal/sofka/Principal.java
@@ -0,0 +1,160 @@
+package punto16.tallerfinal.sofka;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException {
+        Scanner entrada = new Scanner(System.in);
+        /**
+         * En esta seccion se crean los parametros de los 3 objetos que se van a crear
+         */
+        String nombrePersona1;
+        String nombrePersona2;
+        String nombrePersona3;
+
+        int edadPersona1;
+        int edadPersona2;
+        int edadPersona3;
+
+        String sexoPersona1 = "H";
+        String sexoPersona2;
+        String sexoPersona3;
+
+        double pesoPersona1;
+        double pesoPersona2;
+        double pesoPersona3;
+
+        double alturaPersona1;
+        double alturaPersona2;
+        double alturaPersona3;
+
+        /**
+         * Acontinuacion se piden los datos al usuario del
+         * primer objeto que se va a crear y se inicializa el constructor del primer objeto
+         */
+
+        nombrePersona1 = JOptionPane.showInputDialog("Ingresa el nombre de la primera persona: ");
+
+        edadPersona1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la edad de la primera persona: "));
+
+        sexoPersona1 = JOptionPane.showInputDialog("\"Ingresa el sexo con una letra 'H' para hombre 'M' para mujer de la primera persona: \"");
+
+        pesoPersona1 = Double.parseDouble(JOptionPane.showInputDialog("Ingresa el peso de la primera persona: "));
+
+        alturaPersona1 = Double.parseDouble(JOptionPane.showInputDialog("\"Ingresa la altura de la primera persona: \""));
+
+
+        Persona persona1 = new Persona(nombrePersona1, edadPersona1, sexoPersona1, pesoPersona1, alturaPersona1);
+
+        /**
+         * Acontinuacion se inicializa los parametros del segundo objeto, creado con
+         * un constructor que contiene los parametros nombre,edad,sexo
+         */
+
+        nombrePersona2 = JOptionPane.showInputDialog("Ingresa nombre de la segunda persona: ");
+
+        edadPersona2 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa edad de la segunda persona: "));
+
+        sexoPersona2 = JOptionPane.showInputDialog("\"Ingresa el sexo con una letra 'H' para hombre 'M' para mujer de la segunda persona: \"");
+
+
+
+        Persona persona2 = new Persona(nombrePersona2, edadPersona2, sexoPersona2);
+
+        pesoPersona2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso de la segunda persona:"));
+        persona2.setPeso(pesoPersona2);
+
+        alturaPersona2 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura de la segunda persona"));
+        persona2.setAltura(alturaPersona2);
+
+
+
+        /**
+         * Acontinuacion se crea el tercer objeto con el constructor sin parametros
+         */
+
+        Persona persona3 = new Persona();
+
+        nombrePersona3 = JOptionPane.showInputDialog("Ingresa el nombre de la tercera persona: ");
+        persona3.setNombre(nombrePersona3);
+
+        edadPersona3 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa la edad de la tercera persona: "));
+        persona3.setEdad(edadPersona3);
+
+        sexoPersona3 = JOptionPane.showInputDialog("\"Ingresa el sexo con una letra 'H' para hombre 'M' para mujer de la tercera persona: \"");
+        persona3.setSexo(sexoPersona3);
+
+        pesoPersona3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso de la tercera persona:"));
+        persona3.setPeso(pesoPersona3);
+
+        alturaPersona3 = Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura de la segunda persona"));
+        persona3.setAltura(alturaPersona3);
+
+        /**
+         * Se crea una lista para guardar los 3 objetos creados anteriormente
+         */
+
+        ArrayList<Persona> listaPersonas = new ArrayList<Persona>();
+        listaPersonas.add(persona1);
+        listaPersonas.add(persona2);
+        listaPersonas.add(persona3);
+
+        /**
+         * Se crea un menu para conocer que quiere saber el usuario
+         * de las personas creadas
+         */
+
+        int opcionIngresaUsuarioMenu = Integer.parseInt(JOptionPane.showInputDialog("****MENU****\n" +
+                "1-PARA COMPROBAR SI LA PERSONA ESTA EN EL PESO IDEAL\n" +
+                "2-PARA SABER SI LA PERSONA ES MAYOR DE EDAD\n" +
+                "3-MOSTRAR INFORMACION DE LAS 3 PERSONAS\n"));
+
+//        System.out.println("****MENU****\n" +
+//                "1-PARA COMPROBAR SI LA PERSONA ESTA EN EL PESO IDEAL\n" +
+//                "2-PARA SABER SI LA PERSONA ES MAYOR DE EDAD\n" +
+//                "3-MOSTRAR INFORMACION DE LAS 3 PERSONAS\n");
+
+
+        /**
+         * Segun la opcion que el usuario haya escogido, el programa
+         * tomara la desicion correcta para cumplir la peticion del usuario
+         */
+        int pesoideal;
+        switch(opcionIngresaUsuarioMenu){
+            case 1:
+                for(int i=0;i<=2;i++){
+                    pesoideal = listaPersonas.get(i).calcularIMC(listaPersonas.get(i).getPeso(),listaPersonas.get(i).getAltura() );
+                    if(pesoideal==-1){
+                        JOptionPane.showMessageDialog(null, listaPersonas.get(i).getNombre()+" Tiene el peso Ideal");
+                        //System.out.println(listaPersonas.get(i).getNombre()+" Tiene el peso Ideal");
+                    }else if(pesoideal == 0){
+                        JOptionPane.showMessageDialog(null,listaPersonas.get(i).getNombre()+" esta por Debajo de su peso Ideal");
+                        //System.out.println(listaPersonas.get(i).getNombre()+" Tiene esta por Debajo de su peso Ideal");
+                    }else{
+                        JOptionPane.showMessageDialog(null,listaPersonas.get(i).getNombre()+" Tiene SobrePeso");
+                        //System.out.println(listaPersonas.get(i).getNombre()+" Tiene SobrePeso");
+                    }
+                }
+                break;
+            case 2:
+                for(int i=0;i<=2;i++){
+                    if(listaPersonas.get(i).esMayorDeEdad()){
+                        JOptionPane.showMessageDialog(null,listaPersonas.get(i).getNombre() +" Es Mayor de edad");
+                        //System.out.println(listaPersonas.get(i).getNombre() +" Es Mayor de edad" );
+                    }else{
+                        JOptionPane.showMessageDialog(null,listaPersonas.get(i).getNombre() +" Es Menor de edad");
+                        //System.out.println(listaPersonas.get(i).getNombre() +" Es Menor de edad");
+                    }
+                }
+                break;
+            case 3:
+                for(int i=0;i<=2;i++){
+                    System.out.println(listaPersonas.get(i).toString());
+                }
+                break;
+        }
+    }
+}
