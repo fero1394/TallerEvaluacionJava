2bc7efa221385e2f0a0870bd2b29a77e84e75708
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-14 22:59:23 -0500

    Se crea la clase televisor funcional

---
+++ src/punto17/tallerfinal/sofka/Television.java
@@ -0,0 +1,58 @@
+package punto17.tallerfinal.sofka;
+
+public class Television extends Electrodomestico{
+    /**
+     * Atributos
+     */
+    private final double RESOLUCION = 20.0;
+    private final boolean SINTONIZADORTDT = false;
+    private double resolucion;
+    private boolean sintonizadorTDT;
+
+    /**
+     * Constructores
+     */
+    public Television(){
+        super();
+        this.resolucion = RESOLUCION;
+        this.sintonizadorTDT = SINTONIZADORTDT;
+    }
+
+    public Television(double precio, double peso){
+        super(precio,peso);
+        this.resolucion = RESOLUCION;
+        this.sintonizadorTDT = SINTONIZADORTDT;
+    }
+
+    public Television(double precioBase,String color,char consumoEnergetico,double peso,double resolucion,boolean sintonizadorTDT){
+        super(precioBase,color,consumoEnergetico,peso);
+        this.resolucion = resolucion;
+        this.sintonizadorTDT = sintonizadorTDT;
+
+    }
+
+    /**
+     * Metodos Getters
+     */
+    public double getResolucion(){
+        return resolucion;
+    }
+
+    public boolean getSintonizadorTDT(){
+        return sintonizadorTDT;
+    }
+
+    public double precioFinal(){
+        double variablePrecioFinal = super.precioFinal();
+        if(resolucion>40){
+            variablePrecioFinal =+ (variablePrecioFinal*0.30);
+        }
+        if(sintonizadorTDT){
+            variablePrecioFinal =+ 50.0;
+        }
+        return variablePrecioFinal;
+    }
+
+
+
+}
