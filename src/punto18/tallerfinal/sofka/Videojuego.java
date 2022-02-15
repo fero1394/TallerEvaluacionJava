cb04fd188bfd26ca2aafed9f8d3abb6484d1ffbb
FernandoQuicenoG <fqgomez@misena.edu.co>
2022-02-15 00:42:22 -0500

    Se crean atributos de la clase videojuegos

--- src/punto18/tallerfinal/sofka/Videojuego.java
+++ src/punto18/tallerfinal/sofka/Videojuego.java
@@ -1,5 +1,13 @@
 package punto18.tallerfinal.sofka;
 
 public class Videojuego {
-    
+    /**
+     * Atributos
+     */
+    private String titulo;
+    private int horasEstimadas = 10;
+    private boolean entregado = false;
+    private String genero;
+    private String compañia;
+
 }
